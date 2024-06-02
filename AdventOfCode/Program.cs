b125db3b341cab85e8da45a82d043c50035a4726
CrumbsTrace <CrumbsTrace@protonmail.com>
2024-06-02 23:56:01 +0200

    Cleaner RunBenchmarks

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,14 +1,15 @@
 ﻿using AdventOfCode;
 using BenchmarkDotNet.Running;
 
-RunBenchmarks();
+RunBenchmarks(2023);
 return;
 
-static void RunBenchmarks(string benchMarkPattern = ".*", string? year = null)
+static void RunBenchmarks(int year = 2023, int? day = null)
 {
+    var benchMarkPattern = day == null ? ".*" : $".*Day{day:D2}.*";
     var config = new BenchConfig(benchMarkPattern);
     var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-    var yearAssemblies = assemblies.Where(a => a.FullName!.StartsWith("AdventOfCode"));
-    var benchesTypes = yearAssemblies.SelectMany(a => a.GetTypes().Where(t => t.Name == "Benches" && t.Namespace!.Contains(year ?? "")));
-    BenchmarkRunner.Run(benchesTypes.ToArray(), config);
+    var assembly = assemblies.First(a => a.FullName!.StartsWith("AdventOfCode"));
+    var benchmarkType = assembly.GetTypes().First(t => t.FullName!.Contains($"Year{year}.Benches"));
+    BenchmarkRunner.Run(benchmarkType, config);
 }
