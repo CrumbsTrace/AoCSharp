64bdc9af29652f47f8ac75d2cbc1d72c236b7fc0
CrumbsTrace <CrumbsTrace@protonmail.com>
2024-06-03 02:17:34 +0200

    Optimize ParseDigitAtStart

--- AdventOfCode/Year2023/Day01.cs
+++ AdventOfCode/Year2023/Day01.cs
@@ -21,21 +21,24 @@ public static class Day01
         int part1 = 0, part2 = 0;
         Parallel.ForEach(lines, (line, _) =>
         {
-            Interlocked.Add(ref part1, FindResult(line, false));
-            Interlocked.Add(ref part2, FindResult(line, true));
+            Interlocked.Add(ref part1, GetFirstDigit(line, false) * 10 + GetLastDigit(line, false));
+            Interlocked.Add(ref part2, GetFirstDigit(line, true) * 10 + GetLastDigit(line, true));
         });
         return (part1, part2);
     }
 
-    private static int FindResult(ReadOnlySpan<char> line, bool p2) =>
-        GetFirstDigit(line, p2) * 10 + GetLastDigit(line, p2);
+    private static int GetFirstDigit(ReadOnlySpan<char> line, bool p2)
+    {
+        var index = p2 ? line.IndexOfAny(P2Search) : line.IndexOfAny(P1Search);
+        return ParseDigitAtStart(line[index..]);
+    }
 
     private static int GetLastDigit(ReadOnlySpan<char> line, bool p2)
     {
         var index = line.LastIndexOfAny(P1Search);
         if (!p2)
             return line[index] - '0';
-        
+
         //Unfortunately there is no LastIndexOfAny for SearchValues<string>
         //So we have to just jump to the next digit till we reach the end
         while (index < line.Length - 2)
@@ -50,40 +53,17 @@ public static class Day01
         return ParseDigitAtStart(line[index..]);
     }
 
-    private static int GetFirstDigit(ReadOnlySpan<char> line, bool p2)
-    {
-        var index = p2 ? line.IndexOfAny(P2Search) : line.IndexOfAny(P1Search); 
-        return ParseDigitAtStart(line[index..]);
-    }
-
     private static int ParseDigitAtStart(ReadOnlySpan<char> line)
     {
-        foreach (var digit in DigitsP2)
+        return line[0] switch
         {
-            if (!line.StartsWith(digit)) continue;
-            return StringToInt(digit);
-        }
-
-        return 0;
-    }
-
-    private static int StringToInt(ReadOnlySpan<char> span)
-    {
-        if (span.Length == 1)
-            return span[0] - '0';
-
-        return span switch
-        {
-            "one" => 1,
-            "two" => 2,
-            "three" => 3,
-            "four" => 4,
-            "five" => 5,
-            "six" => 6,
-            "seven" => 7,
-            "eight" => 8,
-            "nine" => 9,
-            _ => 0
+            'o' => 1,
+            't' => line[1] == 'w' ? 2 : 3,
+            'f' => line[1] == 'o' ? 4 : 5,
+            's' => line[1] == 'i' ? 6 : 7,
+            'e' => 8,
+            'n' => 9,
+            _ => line[0] - '0'
         };
     }
 }
