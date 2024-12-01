d182f0e01d11b98db4029cf9245083858b1ae158
CrumbsTrace <CrumbsTrace@protonmail.com>
2024-12-01 11:09:43 +0100

    Avoid MemoryAllocations

--- AdventOfCode/Year2024/Day01.cs
+++ AdventOfCode/Year2024/Day01.cs
@@ -1,24 +1,33 @@
 namespace AdventOfCode.Year2024;
+
 public static class Day01
 {
     public static (int, int) Run(string[] input)
     {
-        List<int> list1 = [], list2 = [];
-        foreach(var line in input)
+        List<int> list1 = new(input.Length), list2 = new(input.Length);
+        foreach(var line in input.AsSpan())
         {
-            var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            list1.Add(int.Parse(parts[0]));
-            list2.Add(int.Parse(parts[1]));
+            var enumerator = line.AsSpan().Split("   ");
+            list1.Add(GetNextResult(ref enumerator, line));
+            list2.Add(GetNextResult(ref enumerator, line));
         }
-        list1.Sort();
+        list1.Sort(); 
         list2.Sort();
         
-        //Calculate the differences for each line and sum them up
-        var p1 = list1.Zip(list2).Sum(pair => Math.Abs(pair.First - pair.Second));
-        
-        //Group the counts of each number in list 2
-        var counts = list2.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
-        var p2 = list1.Sum(x => counts.GetValueOrDefault(x) * x);
+        //Take advantage of the fact that the lists are sorted
+        var counts = list2.CountsOrdered();
+        int p1 = 0, p2 = 0;
+        for (var i = 0; i < list1.Count; i++)
+        {
+            p1 += Math.Abs(list1[i] - list2[i]);
+            p2 += counts.GetValueOrDefault(list1[i]) * list1[i];
+        }
         return (p1, p2);
+    }
+    
+    private static int GetNextResult(ref MemoryExtensions.SpanSplitEnumerator<char> enumerator, ReadOnlySpan<char> input)
+    {
+        enumerator.MoveNext();
+        return int.Parse(input[enumerator.Current]);
     }
 }
