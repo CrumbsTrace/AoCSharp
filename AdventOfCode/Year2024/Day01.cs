484f1af6c383067e27d7473523e7f1af7ace50d8
CrumbsTrace <CrumbsTrace@protonmail.com>
2024-12-01 13:27:48 +0100

    Revert "Eh, no allocations is more fun"
    This reverts commit 715ba9709286a642d35ab9c163f4554c752c018c.

--- AdventOfCode/Year2024/Day01.cs
+++ AdventOfCode/Year2024/Day01.cs
@@ -4,8 +4,7 @@ public static class Day01
 {
     public static (int, int) Run(string[] input)
     {
-        Span<int> left = stackalloc int[input.Length];
-        Span<int> right = stackalloc int[input.Length];
+        int[] left = new int[input.Length], right = new int[input.Length];
         for (var i = 0; i < input.Length; i++)
         {
             var line = input[i];
@@ -13,8 +12,8 @@ public static class Day01
             left[i] = GetNextResult(ref enumerator, line);
             right[i] = GetNextResult(ref enumerator, line);
         }
-        left.Sort();
-        right.Sort();
+        left.AsSpan().Sort();
+        right.AsSpan().Sort();
 
         int p1 = 0, p2 = 0, j = 0;
         for (var i = 0; i < left.Length; i++)
